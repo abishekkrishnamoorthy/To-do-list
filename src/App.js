3eb659bcc71b859c0a9a38405d222b0ee95cd311
Abishek krishnamoorthy <102430868+abishekkrishnamoorthy@users.noreply.github.com>
2023-12-06 22:59:58 +0530

    60% finish

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header"
-
+import React, { useState } from "react"
 function App() {
+  const [items,setitem]=useState([{
+    id:1,
+    item:"pawn",
+    checked:true
+},{
+id:2,
+item:"bishop",
+checked:true
+},{
+id:3,
+item:"king",
+checked:false
+},{
+id:4,
+item:"queen",
+checked:false
+}] )
+
+const handlecheck=(id)=>{
+ const listitem=items.map((item)=> item.id===id? {...item,checked:!item.checked}:item)
+ setitem(listitem)
+ localStorage.setItem("to-do-list",JSON.stringify(listitem))
+ 
+}
+const handledlete=(id)=>{
+const listitem=items.filter(item=> item.id !== id).map((item)=> item)
+setitem(listitem)
+localStorage.setItem("to-do-list",JSON.stringify(listitem))
+
+}
+
+
   return (
     <div className="rounded-lg App ">
     <Header />
-    <Content />
-    <Footer />
+    <Content  items={items}
+              handlecheck={handlecheck}
+              handledlete={handledlete}
+              />
+    <Footer item={items}/>
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-const Footer = () => {
+const Footer = ({item}) => {
   return (
-    <footer className="sticky top-0 shadow-lg shadow-cyan-500/50 rounded-lg ">footer</footer>
+    <footer className="sticky top-0 shadow-lg shadow-cyan-500/50 rounded-lg p-3 text-2xl">Task : {item.length}</footer>
   )
 }
 
